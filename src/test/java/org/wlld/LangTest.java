e0622ccfc3a602b1ac9af8231dee13554218e462
lidapeng <ldp@weds.com.cn>
2020-02-28 11:54:57 +0800

    修改版本号

--- src/test/java/org/wlld/LangTest.java
+++ src/test/java/org/wlld/LangTest.java
@@ -20,7 +20,7 @@ public class LangTest {
         TemplateReader templateReader = new TemplateReader();
         templateReader.read("/Users/lidapeng/Desktop/myDocment/a1.txt", "UTF-8", IOConst.NOT_WIN);
         Talk talk = new Talk();
-        List<Integer> list = talk.talk("被锁外面了");
+        List<Integer> list = talk.talk("帮我配把锁");
         System.out.println(list);
     }
 }
