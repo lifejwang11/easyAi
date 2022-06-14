d8dadea6bcd322468d6d83de0dbdaa6b0ac658a2
794757862@qq.com <6205194max>
2022-06-14 16:52:02 +0800

    增加id设置检测重复抛错

--- src/test/java/org/wlld/ImageTest.java
+++ src/test/java/org/wlld/ImageTest.java
@@ -10,17 +10,19 @@ import org.wlld.entity.PicturePosition;
 import org.wlld.entity.ThreeChannelMatrix;
 import org.wlld.tools.Picture;
 
+import javax.jws.WebParam;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class ImageTest {
     public static void main(String[] args) throws Exception {
-        dish();
+        //dish();
+        study();
     }
 
     public static void dish() throws Exception {//识别
-        Picture picture = new Picture();
+        //初始化过程，在服务启动时候 初始化一次 且进行单例化
         Config config = new Config();
         config.setTypeNub(2);//设置类别数量
         config.setBoxSize(125);//设置物体大小 单位像素
@@ -29,6 +31,8 @@ public class ImageTest {
         config.setShowLog(true);//输出学习时打印数据
         Distinguish distinguish = new Distinguish(config);
         distinguish.insertModel(JSONObject.parseObject(ModelData.DATA, Model.class));
+        //识别过程 是直接拿单例化的识别类进行操作 而不进行初始化
+        Picture picture = new Picture();
         for (int i = 1; i < 8; i++) {
             System.out.println("i====" + i);
             ThreeChannelMatrix t = picture.getThreeMatrix("E:\\ls\\fp15\\t" + i + ".jpg");
@@ -40,17 +44,17 @@ public class ImageTest {
     }
 
     public static void study() throws Exception {//学习
-        Picture picture = new Picture();
-        Config config = new Config();
+        Picture picture = new Picture();//图片解析类 图片（文件）-三通道矩阵
+        Config config = new Config();//现有的环境业务进行参数配置
         config.setTypeNub(2);//设置类别数量
-        config.setBoxSize(125);//设置物体大小 单位像素
+        config.setBoxSize(125);//设置物体大小 单位像素 125*125 矩形
         config.setPictureNumber(5);//设置每个种类训练图片数量
-        config.setPth(0.6);//设置可信概率，只有超过可信概率阈值，得出的结果才是可信的
+        config.setPth(0.7);//设置可信概率，只有超过可信概率阈值，得出的结果才是可信的0-1
         config.setShowLog(true);//输出学习时打印数据
-        Distinguish distinguish = new Distinguish(config);
-        distinguish.setBackGround(picture.getThreeMatrix("E:\\ls\\fp15\\back.jpg"));
-        List<FoodPicture> foodPictures = new ArrayList<>();
-        for (int i = 1; i < 3; i++) {
+        Distinguish distinguish = new Distinguish(config);//识别类
+        distinguish.setBackGround(picture.getThreeMatrix("E:\\ls\\fp15\\back.jpg"));//塞入背景图片
+        List<FoodPicture> foodPictures = new ArrayList<>();//所有识别物体的标注集合
+        for (int i = 1; i < 3; i++) {//加载图片全过程
             FoodPicture foodPicture = new FoodPicture();
             foodPictures.add(foodPicture);
             List<PicturePosition> picturePositionList = new ArrayList<>();
@@ -69,8 +73,10 @@ public class ImageTest {
                 picturePositionList.add(picturePosition);
             }
         }
-        distinguish.studyImage(foodPictures);//进行学习
-        System.out.println(JSON.toJSONString(distinguish.getModel()));
+        distinguish.studyImage(foodPictures);//进行学习 耗时较长、、、、
+        Model model = distinguish.getModel();
+        System.out.println(JSON.toJSONString(model));
+        //识别过程
         for (int i = 1; i < 8; i++) {
             System.out.println("i====" + i);
             ThreeChannelMatrix t = picture.getThreeMatrix("E:\\ls\\fp15\\t" + i + ".jpg");
