74e1b6c208ed1a7605f228a8905d7091b74fbe03
794757862@qq.com <6205194max>
2022-09-22 13:32:00 +0800

    优化交互机器人测试案例

--- src/test/java/org/wlld/GameRobotTest.java
+++ src/test/java/org/wlld/GameRobotTest.java
@@ -1,5 +1,6 @@
 package org.wlld;
 
+import org.wlld.MatrixTools.Matrix;
 import org.wlld.gameRobot.Action;
 import org.wlld.gameRobot.DynamicProgramming;
 import org.wlld.gameRobot.DynamicState;
@@ -9,19 +10,19 @@ import java.util.*;
 
 public class GameRobotTest {
     private static DynamicProgramming dynamicProgramming = new DynamicProgramming();//中奖机器人智能机器人(中奖)
-    private static DynamicProgramming dynamicProgrammingTwo = new DynamicProgramming();//不中奖智能机器人(不中奖)
+    private static DynamicProgramming dynamicProgrammingTwo = new DynamicProgramming();//不中奖智能机器人(不中奖即往墙上撞)
 
     //游戏机器人测试
     //这是一个寻找宝藏的游戏，地图是一个6*6大小的格子地图，游戏里有1个宝藏，三个炸弹，每次宝藏和炸弹刷新的位置随机。
     //一个小人从地图的随机一个位置出发，共走六步，如果能拿到指定宝藏就成功。
     // 如果六步没有拿到宝藏就失败，或者六步之内撞到地图边缘同样也是失败，如果碰到炸弹同样失败。
     public static void main(String[] args) throws Exception {
-        GameConfig gameConfig = new GameConfig();
         Random random = new Random();
-        int gameIndex = random.nextInt(4);//设定随机中奖下标，其他下标为炸弹
+        GameConfig gameConfig = new GameConfig();
+        int gameIndex = random.nextInt(8);//设定随机中奖下标，其他下标为炸弹
         gameConfig.setPrizeIndex(gameIndex);
         List<int[]> steps = start(gameConfig, true);//执行游戏 (配置参数，是否获奖)
-        List<int[]> p = gameConfig.getPrizeList();//获取这个
+
     }
 
     private static int isPrize(GameConfig gameConfig, int[] position) {//是否有奖品
@@ -47,7 +48,7 @@ public class GameRobotTest {
             int[] stateID = action.actionTest(position);
             int x = stateID[0];
             int y = stateID[1];
-            if (x < 0 || y < 0 || x > 5 || y > 5) {
+            if (x == 6 && y == 6) {
                 states.add(stateID);
             }
         }
@@ -66,7 +67,7 @@ public class GameRobotTest {
         return isCup;
     }
 
-    private static boolean isBoom(int[] position, GameConfig gameConfig) {//是否踩雷或撞墙
+    private static boolean isBoom(int[] position, GameConfig gameConfig) throws Exception {//是否踩雷或撞墙
         boolean isBoom = false;
         List<int[]> prizeList = gameConfig.getPrizeList();
         //验证是否撞到炸弹
@@ -78,19 +79,23 @@ public class GameRobotTest {
         //撞墙了
         int x = position[0];
         int y = position[1];
-        if ((!isBoom) && (x < 0 || y < 0 || x > 5 || y > 5)) {
+        if ((!isBoom) && x == 6 && y == 6) {
             isBoom = true;
+            throw new Exception("失败======");
         }
         return isBoom;
     }
 
-    private static List<int[]> getBadStrategy(GameConfig gameConfig) {
+    private static List<int[]> getBadStrategy(GameConfig gameConfig) throws Exception {
         List<int[]> steps = new ArrayList<>();
         Random random = new Random();
         int[] startPosition;
+        Matrix matrix = dynamicProgrammingTwo.getValueMatrix();
+        double value;
         do {
             startPosition = new int[]{random.nextInt(6), random.nextInt(6)};
-        } while (isFinish(gameConfig, startPosition));
+            value = matrix.getNumber(startPosition[1], startPosition[0]);
+        } while (value <= 0);
         int step = 6;//步数
         int[] actionId = startPosition;
         steps.add(startPosition);
@@ -122,15 +127,18 @@ public class GameRobotTest {
         return steps;
     }
 
-    private static List<int[]> getBestStrategy(GameConfig gameConfig) {
+    private static List<int[]> getBestStrategy(GameConfig gameConfig) throws Exception {
         Random random = new Random();
         List<int[]> steps = new ArrayList<>();
+        Matrix matrix = dynamicProgramming.getValueMatrix();
         do {
             steps.clear();
             int[] startPosition;
+            double value;
             do {
                 startPosition = new int[]{random.nextInt(6), random.nextInt(6)};
-            } while (isFinish(gameConfig, startPosition));
+                value = matrix.getNumber(startPosition[1], startPosition[0]);
+            } while (value <= 0);//起点的价值必须大于0，因为价值为0是终结态，价值小于0 的状态四周被终结态围堵。
             int step = 12;//步数
             int[] actionId = startPosition;
             steps.add(startPosition);
@@ -158,30 +166,59 @@ public class GameRobotTest {
                 }
                 steps.add(actionId);
             }
-        } while ((steps.size() - 1) > 6);
+        } while ((steps.size() - 1) > 12);
         return steps;
     }
 
     //配置参数，是否中奖
-    public static List<int[]> start(GameConfig gameConfig, boolean isWinPrize) {//初始化数据
+    public static List<int[]> start(GameConfig gameConfig, boolean isWinPrize) throws Exception {//初始化数据
         List<int[]> steps;
         Random random = new Random();
         //初始化随机奖品位置
         List<int[]> prizeList = gameConfig.getPrizeList();//生成随机四个奖品坐标
-        int[] cup = new int[]{random.nextInt(3) + 3, random.nextInt(3) + 3};
-        for (int i = 0; i < 4; i++) {
+        int[] position = new int[0];
+        int[] myPosition;
+        for (int i = 0; i < 8; i++) {//设置8个坐标集合
             switch (i) {
                 case 0:
-                    prizeList.add(new int[]{random.nextInt(3), random.nextInt(3)});
+                    position = new int[]{random.nextInt(3), random.nextInt(3)};
+                    prizeList.add(position);
                     break;
                 case 1:
-                    prizeList.add(new int[]{random.nextInt(3) + 3, random.nextInt(3)});
+                    do {
+                        myPosition = new int[]{random.nextInt(3), random.nextInt(3)};
+                    } while (Arrays.equals(myPosition, position));
+                    prizeList.add(myPosition);
                     break;
                 case 2:
-                    prizeList.add(cup);
+                    position = new int[]{random.nextInt(3) + 3, random.nextInt(3)};
+                    prizeList.add(position);
                     break;
                 case 3:
-                    prizeList.add(new int[]{random.nextInt(3), random.nextInt(3) + 3});
+                    do {
+                        myPosition = new int[]{random.nextInt(3) + 3, random.nextInt(3)};
+                    } while (Arrays.equals(myPosition, position));
+                    prizeList.add(myPosition);
+                    break;
+                case 4:
+                    position = new int[]{random.nextInt(3) + 3, random.nextInt(3) + 3};
+                    prizeList.add(position);
+                    break;
+                case 5:
+                    do {
+                        myPosition = new int[]{random.nextInt(3) + 3, random.nextInt(3) + 3};
+                    } while (Arrays.equals(myPosition, position));
+                    prizeList.add(myPosition);
+                    break;
+                case 6:
+                    position = new int[]{random.nextInt(3), random.nextInt(3) + 3};
+                    prizeList.add(position);
+                    break;
+                case 7:
+                    do {
+                        myPosition = new int[]{random.nextInt(3), random.nextInt(3) + 3};
+                    } while (Arrays.equals(myPosition, position));
+                    prizeList.add(myPosition);
                     break;
             }
         }
@@ -201,6 +238,13 @@ public class GameRobotTest {
                 dynamicStateListTwo.add(dynamicState2);
             }
         }
+        int[] state = new int[]{6, 6};//创建一个撞墙终结态，即一旦撞墙，则自动跳转到[6,6]的状态
+        DynamicState dynamicState = new DynamicState(state);
+        DynamicState dynamicState2 = new DynamicState(state);
+        dynamicState.setFinish(true);
+        dynamicState2.setFinish(true);
+        dynamicStateList.add(dynamicState);
+        dynamicStateListTwo.add(dynamicState2);
         //加载动作
         Map<Integer, Action> actionMap = dynamicProgramming.getActionMap();
         Map<Integer, Action> actionMapTwo = dynamicProgrammingTwo.getActionMap();
@@ -228,9 +272,16 @@ public class GameRobotTest {
         actionMapTwo.put(2, right2);
         actionMapTwo.put(3, up2);
         actionMapTwo.put(4, down2);
+        List<int[]> p = gameConfig.getPrizeList();//获取这个
+        int size = p.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println("i==" + i + ":" + Arrays.toString(p.get(i)));
+        }
+        System.out.println("中奖下标：" + gameConfig.getPrizeIndex());
         if (isWinPrize) {
             dynamicProgramming.gameStart();//探索中奖
             dynamicProgramming.strategyStudy();//研究策略中奖
+            System.out.println(dynamicProgramming.getValueMatrix().getString());
             steps = getBestStrategy(gameConfig);
         } else {
             dynamicProgrammingTwo.gameStart();//探索不中奖
