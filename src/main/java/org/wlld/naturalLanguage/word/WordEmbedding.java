599cf8415f186acd73cd2b7437ded3b8b5898084
lidapeng <794757862@qq.com>
2024-09-25 11:02:33 +0800

    词向量嵌入器不在循环训练

--- src/main/java/org/wlld/naturalLanguage/word/WordEmbedding.java
+++ src/main/java/org/wlld/naturalLanguage/word/WordEmbedding.java
@@ -31,10 +31,8 @@ public class WordEmbedding {
     private final List<String> wordList = new ArrayList<>();//单字集合
     private SentenceConfig config;
     private int wordVectorDimension;
-    private int times;
 
     public void setConfig(SentenceConfig config) {
-        times = config.getTimes();
         this.config = config;
     }
 
@@ -112,17 +110,13 @@ public class WordEmbedding {
         List<String> sentenceList = sentenceModel.getSentenceList();
         int size = sentenceList.size();
         System.out.println("词嵌入训练启动...");
-        for (int t = 0; t < times; t++) {
-            int index = 0;
-            for (int i = index; i < size; i++) {
-                long start = System.currentTimeMillis();
-                study(sentenceList.get(i));
-                long end = System.currentTimeMillis() - start;
-                index++;
-                double r = ((index * (t + 1)) / (double) (size * times)) * 100;
-                String result = String.format("%.6f", r);
-                System.out.println("size:" + size + ",index:" + index + ",耗时:" + end + ",完成度:" + result + "%");
-            }
+        for (int i = 0; i < size; i++) {
+            long start = System.currentTimeMillis();
+            study(sentenceList.get(i));
+            long end = System.currentTimeMillis() - start;
+            double r = (double) i / size * 100;
+            String result = String.format("%.6f", r);
+            System.out.println("size:" + size + ",index:" + i + ",耗时:" + end + ",完成度:" + result + "%");
         }
         WordTwoVectorModel wordTwoVectorModel = new WordTwoVectorModel();
         wordTwoVectorModel.setModelParameter(nerveManager.getModelParameter());
