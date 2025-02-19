b16006fa14450181d23f57b5a53c7064b98f0856
aoshiguchen <1052045476@qq.com>
2025-02-19 17:42:17 +0800

    增加cuda扩展接口.

---
+++ src/main/java/org/dromara/easyai/matrixTools/CudaMatrix.java
@@ -0,0 +1,22 @@
+package org.dromara.easyai.matrixTools;
+
+public interface CudaMatrix {
+	void init() throws Exception;
+	void softMax(Matrix matrix) throws Exception;
+
+	Matrix matrixSoftMaxPd(Matrix qkt, Matrix errorMatrix, double wordVectorDimension) throws Exception;
+
+	Matrix mulMatrix(Matrix matrix1, Matrix matrix2) throws Exception;
+
+	// 矩阵数加
+	void mathAdd(Matrix matrix, double nub) throws Exception;
+
+	// 矩阵数减
+	void mathSub(Matrix matrix, double nub) throws Exception;
+
+	// 矩阵数乘
+	void mathMul(Matrix matrix, double nub) throws Exception;
+
+	// 矩阵数除
+	void mathDiv(Matrix matrix, double nub) throws Exception;
+}
