b976fa4fb96d57559239e9d614c020f2d9a3c850
lidapeng <794757862@qq.com>
2025-04-01 22:37:07 +0800

    准备TransFormer内置词向量4

--- src/main/java/org/dromara/easyai/transFormer/seflAttention/MultiSelfAttention.java
+++ src/main/java/org/dromara/easyai/transFormer/seflAttention/MultiSelfAttention.java
@@ -188,7 +188,7 @@ public class MultiSelfAttention {//多头自注意力层
                 } else {//当列数是奇数
                     pe = (float) Math.cos(wk * i);
                 }
-                float value = feature.getNumber(i, j) + pe * 0.1f;
+                float value = feature.getNumber(i, j) + pe;
                 feature.setNub(i, j, value);
             }
         }
@@ -233,7 +233,7 @@ public class MultiSelfAttention {//多头自注意力层
         matrixOperation = new MatrixOperation(coreNumber);
         this.transWordVector = transWordVector;
         this.selfTimeCode = selfTimeCode;
-        this.maxLength = maxLength * 10;
+        this.maxLength = maxLength * 2;
         this.codecBlock = codecBlock;
         this.encoder = encoder;
         int yiZhi = wordVectorDimension * multiNumber;
