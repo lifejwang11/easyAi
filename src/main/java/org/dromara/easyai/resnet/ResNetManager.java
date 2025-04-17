545a717e042ac44cb2bfd709590ec981428e9b68
lidapeng <794757862@qq.com>
2025-04-17 13:15:36 +0800

    更新resnet4

--- src/main/java/org/dromara/easyai/resnet/ResNetManager.java
+++ src/main/java/org/dromara/easyai/resnet/ResNetManager.java
@@ -4,6 +4,10 @@ import org.dromara.easyai.config.ResNetConfig;
 import org.dromara.easyai.conv.ResConvCount;
 import org.dromara.easyai.i.ActiveFunction;
 import org.dromara.easyai.nerveCenter.NerveManager;
+import org.dromara.easyai.nerveEntity.SensoryNerve;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author lidapeng
@@ -12,21 +16,48 @@ import org.dromara.easyai.nerveCenter.NerveManager;
  */
 public class ResNetManager extends ResConvCount {
     private final NerveManager nerveManager;
-    private final ResNetConnectionLine resNetConnectionLine = new ResNetConnectionLine();
-    private final float studyRate;//全局学习率
+    private final List<ResBlock> resBlockList = new ArrayList<>();//残差集合
+    private final RestNetInput restNetInput;
+
+    public RestNetInput getRestNetInput() {
+        return restNetInput;
+    }
 
     public ResNetManager(ResNetConfig resNetConfig, ActiveFunction activeFunction) throws Exception {
-        int deep = getConvDeep(resNetConfig.getSize(), resNetConfig.getMinFeatureSize());
-        int lastSize = getFeatureSize(deep, resNetConfig.getSize(), false);
+        int deep = getConvDeep(resNetConfig.getSize(), resNetConfig.getMinFeatureSize());//获取深度
         int channelNo = resNetConfig.getChannelNo();//通道数
-        studyRate = resNetConfig.getStudyRate();
+        int lastSize = getFeatureSize(deep, resNetConfig.getSize(), true);//最后一层特征大小
+        //全局学习率
+        float studyRate = resNetConfig.getStudyRate();
         if (deep < 2) {
-            throw new Exception("图像太小了，不能用resnet进行训练");
+            throw new Exception("图像尺寸太小了，不能用resnet进行训练");
         }
         int featureLength = (int) (channelNo * Math.pow(2, deep - 1));//卷积层输出特征大小
         nerveManager = new NerveManager(featureLength, resNetConfig.getHiddenNerveNumber(), resNetConfig.getTypeNumber(), resNetConfig.getHiddenDeep()
                 , activeFunction, studyRate, resNetConfig.getRegularModel(), resNetConfig.getRegular(), 0);
+        ResNetConnectionLine resNetConnectionLine = new ResNetConnectionLine();
         nerveManager.init(true, resNetConfig.isShowLog(), resNetConfig.isSoftMax(), resNetConnectionLine);
-
+        for (int i = 0; i < deep; i++) {
+            List<SensoryNerve> sensoryNerves = null;
+            if (i == deep - 1) {
+                sensoryNerves = nerveManager.getSensoryNerves();
+            }
+            ResBlock resBlock = new ResBlock(channelNo, i + 1, studyRate, resNetConfig.getSize(), sensoryNerves);
+            resBlockList.add(resBlock);
+        }
+        restNetInput = new RestNetInput(resBlockList.get(0), resNetConfig.getSize());
+        connection();//残差块进行互相连接
+        resNetConnectionLine.setLastBlock(resBlockList.get(deep - 1), lastSize, resNetConfig.getHiddenNerveNumber(), featureLength);
     }
+
+    private void connection() {//残差块相互连接
+        int size = resBlockList.size();
+        for (int i = 0; i < size - 1; i++) {
+            ResBlock resBlock = resBlockList.get(i);
+            ResBlock nextResBlock = resBlockList.get(i + 1);
+            resBlock.setSonResBlock(nextResBlock);
+            nextResBlock.setFatherResBlock(resBlock);
+        }
+    }
+
 }
---
+++ src/main/java/org/dromara/easyai/resnet/RestNetInput.java
@@ -0,0 +1,40 @@
+package org.dromara.easyai.resnet;
+
+import org.dromara.easyai.entity.ThreeChannelMatrix;
+import org.dromara.easyai.i.OutBack;
+import org.dromara.easyai.matrixTools.Matrix;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author lidapeng
+ * @time 2025/4/17 13:04
+ * @des restNet输出类
+ */
+public class RestNetInput {
+    private final ResBlock fistBlock;//第一个残差块
+    private final int imageSize;//图像大小
+
+    public RestNetInput(ResBlock fistBlock, int imageSize) {
+        this.fistBlock = fistBlock;
+        this.imageSize = imageSize;
+    }
+
+    public void postThreeChannelMatrix(ThreeChannelMatrix threeChannelMatrix, boolean study, Map<Integer, Float> E, OutBack outBack
+            , long eventID) throws Exception {
+        int x = threeChannelMatrix.getX();
+        int y = threeChannelMatrix.getY();
+        if (x == y && x == imageSize) {
+            List<Matrix> matrixList = new ArrayList<>();
+            matrixList.add(threeChannelMatrix.getMatrixR());
+            matrixList.add(threeChannelMatrix.getMatrixG());
+            matrixList.add(threeChannelMatrix.getMatrixB());
+            fistBlock.sendMatrixList(matrixList, outBack, study, E, eventID);
+        } else {
+            throw new Exception("输入的图像必须为正方形，且尺寸必须等同于初始化配置的值：" + imageSize);
+        }
+    }
+
+}
